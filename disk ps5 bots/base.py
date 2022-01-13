10061905b6fb72afbcbe16e1bb60194fb6995a77
Yael Rashlin <58134106+yaelRashlin@users.noreply.github.com>
2022-01-13 17:13:06 +0200

    Add files via upload

---
+++ disk ps5 bots/base.py
@@ -0,0 +1,76 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import configparser as configparser
+import os
+import smtplib
+from datetime import date
+import playsound
+import winsound
+import os
+
+file_loc = os.path.realpath(__file__)
+script_dir = os.path.dirname(file_loc)
+
+# ------------------------------
+# selenium_init:
+# ------------------------------
+class selenium_init():
+    def __init__(self, shop_name) -> None:
+        self.options = Options()
+        self.options.add_argument("user-data-dir=C:\\Users\\ps5\\AppData\\Local\\Google\\Chrome\\User Data\\%s" % shop_name)
+        self.driver = webdriver.Chrome('chromedriver.exe', options = self.options)
+
+        ini = os.path.join(script_dir, "run.ini" )
+        self.config = configparser.ConfigParser()
+        self.config.read(ini)
+
+        self.user = self.config.get(shop_name, 'user')
+        self.pss = self.config.get(shop_name, 'pass')
+        self.url = self.config.get(shop_name, 'item_url')
+        self.id = self.config.get(shop_name, 'id')
+        self.sleep = int(self.config.get("general", 'sleep'))
+        if self.config.get("general", 'send_email') == "on":
+            self.send_email = True
+        else:
+            self.send_email = False
+        self.script_dir = script_dir
+
+    # ------------------------------
+    # print_debug: 
+    # ------------------------------
+    def print_debug(self, item):
+        today = date.today()
+        print("%s -> %s" % (today, item))
+
+    # ------------------------------
+    # play_sound: 
+    # ------------------------------
+    def play_sound(self):
+        print ("Play Hallelujah :) ")
+        winsound.Beep(899, 5000) # Makes a beep sound for 5 seconds to notify the user that the item is in stock 
+        # playsound.playsound(os.path.join(self.script_dir, self.config.get("general", 'mp3') ), True)  
+
+    # ------------------------------
+    # send_email:
+    # ------------------------------
+    def send_email(msg = None, shop_name = None):
+        print (msg)
+        sender = self.config.get("email", 'sender')
+        receivers = self.config.get("email", 'receivers').split(",")
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.login(sender, self.config.get("email", 'token'))
+
+        message = """From: From Person <from@fromdomain.com>
+To: To Person <to@todomain.com>
+MIME-Version: 1.0
+Content-type: text/html
+Subject: Found PS5 at %s
+
+This is an e-mail message to be sent in HTML format
+
+<b>This is HTML message.</b>
+<h1>%s</h1>
+""" % (shop_name, msg)
+
+        server.sendmail(sender, sender, message)
+        server.quit()
---
+++ disk ps5 bots/ivory.py
@@ -0,0 +1,50 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from base import selenium_init
+
+# ------------------------------
+# ivory:
+# ------------------------------
+class ivory(selenium_init):
+    def __init__(self) -> None:
+        selenium_init.__init__(self, ivory.__name__)
+
+    def test_ivory(self):
+        self.driver.set_window_size(960, 784)
+        self.driver.find_element(By.LINK_TEXT, "כניסה").click()
+        self.driver.find_element(By.ID, "mail").click()
+        self.driver.find_element(By.ID, "mail").send_keys(self.user)
+        self.driver.find_element(By.ID, "pass").send_keys(self.pss)
+        self.driver.find_element(By.ID, "btn-invitation").click()
+
+    # ------------------------------
+    # run_bot: 
+    # ------------------------------
+    def run_bot(self):
+        item = self.url.replace("$id", self.id) # Variable for the item wanted
+        print (item)
+        self.driver.get(item)
+        while True:
+            try:
+                self.driver.find_element(By.CSS_SELECTOR, ".col-sm-6 > #fastBuy").click() # Looks for the 'add to cart button' using xpath
+            except:
+                time.sleep(self.sleep)
+                self.print_debug(item)
+                self.driver.get(item) # If the atc button cannot be found, refresh until it is found
+            else:
+                time.sleep(1)
+                self.test_ivory()
+                break
+        self.play_sound()
+        if self.send_email:
+            self.send_email(msg = "GO to ivory at: https://www.olsale.co.il/CheckOutUpsells.aspx?auction_id=%s&f=1" % self.id, shop_name = "ivory")
+
+# --------
+# main:
+# --------
+if(__name__ == "__main__"):
+    print("This program comes with ABSOLUTELY NO WARRANTY; for details check included license. This is free software, and you are welcome to redistribute it under certain conditions; check license for conditions.")
+    o = ivory()
+    o.run_bot()
